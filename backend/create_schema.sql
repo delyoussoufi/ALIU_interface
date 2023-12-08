02895b9440167145470279f68136bf74635e5e7a
Dalal <dalal.elyoussoufi@gmail.com>
2023-12-09 00:20:18 +0100

    Create the database schema SQL script.

---
+++ backend/create_schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS t_art_objects (
+    ArtObjectID TEXT PRIMARY KEY,
+    ArtObjectName TEXT,
+    ArtObjectDescription TEXT,
+    OwnedByID TEXT,
+    OwnedByName TEXT,
+    ImageURL TEXT,
+    CreatorName TEXT,
+    DateOfCreation TEXT, -- Stored as TEXT to accommodate various date formats
+    Material TEXT,
+    Location TEXT,
+    Height DECIMAL, -- Assuming height is in a consistent unit like cm or inches
+    Width DECIMAL,  -- Assuming width is in a consistent unit like cm or inches
+    Genre TEXT,
+    Event TEXT,
+    Source TEXT,
+    Origin TEXT,
+    Exhibition TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS t_art_owners (
+    ArtObjectID TEXT REFERENCES t_art_objects(ID),
+    OwnerID TEXT,
+    OwnerName TEXT,
+    OwnershipFrom TEXT, 
+    OwnershipUntil TEXT, 
+    OwnerDescription TEXT,
+    OwnerType TEXT,
+    AcquisitionMethod TEXT,
+    PRIMARY KEY (ArtObjectID, OwnerID),
+    UNIQUE(ArtObjectID, OwnerID, OwnershipFrom)
+);
