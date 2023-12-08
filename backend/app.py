3aacb1b5a1507ab2f797ff29b769e0461cc506b5
Dalal <dalal.elyoussoufi@gmail.com>
2023-12-09 00:56:13 +0100

    Added a route named /artobjects/ to handle GET requests for fetching art objects, it accepts an optional query parameter for searching art objects.

--- backend/app.py
+++ backend/app.py
@@ -7,3 +7,30 @@ CORS(app)
 
 def get_db_connection():
     return sqlite_connect('artdatabase.db')
+
+@app.route('/artobjects/', methods=['GET'])
+def get_art_objects():
+    query = request.args.get('query', '')  # Get the query parameter
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    if query:
+        # Modify this query to search across multiple fields
+        search_query = f"%{query}%"
+        cur.execute("""
+            SELECT * FROM t_art_objects 
+            WHERE ArtObjectName LIKE ?
+            OR ArtObjectID LIKE ?
+            OR ArtObjectDescription LIKE ?
+            OR CreatorName LIKE ?
+
+            """, (search_query, search_query, search_query, search_query))
+    else:
+        cur.execute('SELECT * FROM t_art_objects')
+
+    art_objects = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    art_objects_list = [dict(zip([column[0] for column in cur.description], ao)) for ao in art_objects]
+    return jsonify(art_objects_list)
