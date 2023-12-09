d7fd1a612e7e517fcde0082775be9b726b1293cf
Dalal <dalal.elyoussoufi@gmail.com>
2023-12-09 19:47:36 +0100

    Set up routing and configure the routes.

--- frontend/aliu-interface/src/app/app-routing.module.ts
+++ frontend/aliu-interface/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
+import { ReportsComponent } from './reports/reports.component';
+import { ArtObjectListComponent } from './art-object-list/art-object-list.component';
+import { ArtObjectDetailComponent } from './art-object-detail/art-object-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'art-objects', component: ArtObjectListComponent },
+  { path: 'art-object/:id', component: ArtObjectDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
