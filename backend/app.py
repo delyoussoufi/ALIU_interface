22d7b19e7856e9e729fecc17107eba259dde2954
Dalal <dalal.elyoussoufi@gmail.com>
2023-12-09 16:55:34 +0100

    Updated /artobjects/ route to handle errors.

--- backend/app.py
+++ backend/app.py
@@ -10,30 +10,35 @@ def get_db_connection():
 
 @app.route('/artobjects/', methods=['GET'])
 def get_art_objects():
-    query = request.args.get('query', '')  # Get the query parameter
-    conn = get_db_connection()
-    cur = conn.cursor()
+    try:
+        query = request.args.get('query', '')  # Get the query parameter
+        conn = get_db_connection()
+        cur = conn.cursor()
 
-    if query:
-        # Modify this query to search across multiple fields
-        search_query = f"%{query}%"
-        cur.execute("""
-            SELECT * FROM t_art_objects 
-            WHERE ArtObjectName LIKE ?
-            OR ArtObjectID LIKE ?
-            OR ArtObjectDescription LIKE ?
-            OR CreatorName LIKE ?
+        if query:
+            search_query = f"%{query}%"
+            cur.execute("""
+                SELECT * FROM t_art_objects 
+                WHERE ArtObjectName LIKE ?
+                OR ArtObjectID LIKE ?
+                OR ArtObjectDescription LIKE ?
+                OR CreatorName LIKE ?
+                """, (search_query, search_query, search_query, search_query))
+        else:
+            cur.execute('SELECT * FROM t_art_objects')
 
-            """, (search_query, search_query, search_query, search_query))
-    else:
-        cur.execute('SELECT * FROM t_art_objects')
+        art_objects = cur.fetchall()
+        cur.close()
+        conn.close()
 
-    art_objects = cur.fetchall()
-    cur.close()
-    conn.close()
+        if art_objects:
+            art_objects_list = [dict(zip([column[0] for column in cur.description], ao)) for ao in art_objects]
+            return jsonify(art_objects_list)
+        else:
+            return jsonify({"error": "No art objects found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-    art_objects_list = [dict(zip([column[0] for column in cur.description], ao)) for ao in art_objects]
-    return jsonify(art_objects_list)
 
 @app.route('/ownerships/<art_object_id>', methods=['GET'])
 def get_ownerships(art_object_id):
