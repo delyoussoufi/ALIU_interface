6af4db88dfc1c50a1c024a5792cabb3af2757136
Dalal <dalal.elyoussoufi@gmail.com>
2023-12-09 20:30:34 +0100

    Implemented app.module.ts and app-routing.module.ts configurations and built the search component.

--- frontend/aliu-interface/src/app/app.module.ts
+++ frontend/aliu-interface/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { ArtObjectListComponent } from './art-object-list/art-object-list.component';
 import { ArtObjectDetailComponent } from './art-object-detail/art-object-detail.component';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ReportsComponent } from './reports/reports.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +19,14 @@ import { ReportsComponent } from './reports/reports.component';
     ArtObjectListComponent,
     ArtObjectDetailComponent,
     HeaderComponent,
-    FooterComponent,
     AboutComponent,
-    ReportsComponent
+    ReportsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- frontend/aliu-interface/src/app/search/search.component.ts
+++ frontend/aliu-interface/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  searchTerm: string = '';
 
+  constructor(private router: Router) { }
+
+  searchArtObjects(): void {
+    if (this.searchTerm) {
+      this.router.navigate(['/art-objects'], { queryParams: { query: this.searchTerm } });
+    }
+  }
 }
