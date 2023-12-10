e8dd0d2a74ef76013c4560aad411959d335691ad
Dalal <dalal.elyoussoufi@gmail.com>
2023-12-10 12:58:23 +0100

    Simplify first query to obtain the same objects in the same order.

--- backend/sparql_queries.py
+++ backend/sparql_queries.py
@@ -20,37 +20,18 @@ def fetch_data_with_retries(sparql, max_retries=5, initial_backoff=2):
 def fetch_art_objects():
     sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
     sparql.setQuery("""
-    SELECT ?ArtObjectID ?ArtObjectName ?ArtObjectDescription ?OwnedByID ?OwnedByName ?ImageURL
-        ?CreatorName ?DateOfCreation ?Material ?Location ?Height ?Width ?Genre ?Event ?Source ?Origin ?Exhibition
+    SELECT ?artObject ?artObjectLabel ?artObjectDescription ?ownedby ?ownedbyLabel ?creationDate ?artist ?artistLabel
     WHERE {
-        ?artObject wdt:P31 wd:Q3305213;  # Instances of painting
-                wdt:P127 ?ownedby;       # Known owner
-                wdt:P18 ?image.          # Image of the art object
-
-        ?ownedby wdt:P1840 wd:Q30335959.  # Owner was ALIU Red Flag
-
-        BIND(STRAFTER(STR(?artObject), "http://www.wikidata.org/entity/") AS ?ArtObjectID)
-        OPTIONAL { ?artObject rdfs:label ?ArtObjectName. FILTER(LANG(?ArtObjectName) = "en") }
-        OPTIONAL { ?artObject schema:description ?ArtObjectDescription. FILTER(LANG(?ArtObjectDescription) = "en") }
-        OPTIONAL { ?ownedby rdfs:label ?OwnedByName. FILTER(LANG(?OwnedByName) = "en") }
-
-        OPTIONAL { ?artObject wdt:P571 ?DateOfCreation. }
-        OPTIONAL { ?artObject wdt:P170 ?creator. ?creator rdfs:label ?CreatorName. FILTER(LANG(?CreatorName) = "en") }
-        OPTIONAL { ?artObject wdt:P186 ?material. ?material rdfs:label ?Material. FILTER(LANG(?Material) = "en") }
-        OPTIONAL { ?artObject wdt:P276 ?location. ?location rdfs:label ?Location. FILTER(LANG(?Location) = "en") }
-        OPTIONAL { ?artObject wdt:P2048 ?Height. }
-        OPTIONAL { ?artObject wdt:P2049 ?Width. }
-        OPTIONAL { ?artObject wdt:P136 ?genre. ?genre rdfs:label ?Genre. FILTER(LANG(?Genre) = "en") }
-        OPTIONAL { ?artObject wdt:P793 ?event. ?event rdfs:label ?Event. FILTER(LANG(?Event) = "en") }
-        OPTIONAL { ?artObject wdt:P1343 ?source. ?source rdfs:label ?Source. FILTER(LANG(?Source) = "en") }
-        OPTIONAL { ?artObject wdt:P495 ?origin. ?origin rdfs:label ?Origin. FILTER(LANG(?Origin) = "en") }
-        OPTIONAL { ?artObject wdt:P608 ?exhibition. ?exhibition rdfs:label ?Exhibition. FILTER(LANG(?Exhibition) = "en") }
+        ?artObject wdt:P31 wd:Q3305213 ; # Get items that are instances of painting
+        wdt:P127 ?ownedby. #at least one owner is known in Wikidata
+        ?ownedby wdt:P1840 wd:Q30335959 . #the owner was ALIU Red Flag   
+        OPTIONAL { ?artObject wdt:P571 ?creationDate. } # Get the date of creation of the art object
+        OPTIONAL { ?artObject wdt:P170 ?artist. } # Get the artist of the painting
 
         SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]"}
     }
-    GROUP BY ?ArtObjectID ?ArtObjectName ?ArtObjectDescription ?OwnedByID ?OwnedByName ?ImageURL
-            ?CreatorName ?DateOfCreation ?Material ?Location ?Height ?Width ?Genre ?Event ?Source ?Origin ?Exhibition
-    ORDER BY ?ArtObjectID
+    GROUP BY ?artObject ?artObjectLabel ?artObjectDescription ?ownedby ?ownedbyLabel ?creationDate ?artist ?artistLabel
+    ORDER BY ?item
     LIMIT 100
 
     """)
